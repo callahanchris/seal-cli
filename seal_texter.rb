286ccf74410df1f5e2228afd591f7d54f854e1fc
Chris Callahan <callahan.c@gmail.com>
2014-09-27 01:39:47 -0400

    allows user to choose to send a lyric or fact

--- seal_texter.rb
+++ seal_texter.rb
@@ -2,10 +2,10 @@ require 'rubygems'
 require 'mechanize'
 
 class SealTexter
-  attr_reader :phone
+  attr_reader :phone, :message
 
-  def initialize(phone)
-    @phone = phone
+  def initialize(phone, message)
+    @phone, @message = phone, message
   end
 
   def text_lyrics
@@ -16,13 +16,14 @@ class SealTexter
     a.get('http://www.textfromarose.com/') do |page|
       form = page.forms.first
       form.field_with(name: 'phone_number').value = phone
+      form.fields.last.value = message
       form.click_button
     end
   end
 end
 
-class CLIRunner
-  attr_accessor :phone
+class SealCLI
+  attr_accessor :phone, :message
 
   def run
     puts 'Enter a phone #'
@@ -33,13 +34,29 @@ class CLIRunner
     self.phone = gets.strip
     self.phone = phone.gsub(/[^0-9]/, '')
 
-    [10, 11].include?(phone.length) ? text : run
+    [10, 11].include?(phone.length) ? choose_lyric_or_fact : run
+  end
+
+  def choose_lyric_or_fact
+    puts 'Would you like to send a Seal _lyric_ or a Seal _fact_?'
+    input = gets.chomp
+    case
+    when input =~ /lyric/i
+      self.message = 'lyric'
+      text
+    when input =~ /fact/i
+      self.message = 'fact'
+      text
+    else
+      choose_lyric_or_fact
+    end
   end
 
   def text
-    puts "Texting #{phone}..."
-    SealTexter.new(phone).text_lyrics
+    puts "BAY-BEHHH"
+    puts "Texting #{phone} an essential Seal #{message}!"
+    SealTexter.new(phone, message).text_lyrics
   end
 end
 
-CLIRunner.new.run
+SealCLI.new.run
