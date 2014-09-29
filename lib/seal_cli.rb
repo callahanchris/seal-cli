9c8419965a171e51acee8cfaf10729de68e9d0e9
Chris Callahan <callahan.c@gmail.com>
2014-09-28 22:21:19 -0400

    make most SealCLI methods private

--- lib/seal_cli.rb
+++ lib/seal_cli.rb
@@ -6,31 +6,32 @@ class SealCLI
     verify_phone_num
   end
 
-  def verify_phone_num
-    self.phone = gets.strip
-    self.phone = phone.gsub(/[^0-9]/, '')
+  private
+    def verify_phone_num
+      self.phone = gets.strip
+      self.phone = phone.gsub(/[^0-9]/, '')
 
-    [10, 11].include?(phone.length) ? choose_lyric_or_fact : run
-  end
-
-  def choose_lyric_or_fact
-    puts 'Would you like to send a Seal _lyric_ or a Seal _fact_?'
-    input = gets.chomp
-    case
-    when input =~ /lyric/i
-      self.message = 'lyric'
-      text
-    when input =~ /fact/i
-      self.message = 'fact'
-      text
-    else
-      choose_lyric_or_fact
+      [10, 11].include?(phone.length) ? choose_lyric_or_fact : run
     end
-  end
 
-  def text
-    puts "BAY-BEHHH"
-    puts "Texting #{phone} an essential Seal #{message}!"
-    SealTexter.new(phone, message).text_lyrics
-  end
+    def choose_lyric_or_fact
+      puts 'Would you like to send a Seal _lyric_ or a Seal _fact_?'
+      input = gets.chomp
+      case
+      when input =~ /lyric/i
+        self.message = 'lyric'
+        text
+      when input =~ /fact/i
+        self.message = 'fact'
+        text
+      else
+        choose_lyric_or_fact
+      end
+    end
+
+    def text
+      puts "BAY-BEHHH"
+      puts "Texting #{phone} an essential Seal #{message}!"
+      SealTexter.new(phone, message).text_lyrics
+    end
 end
